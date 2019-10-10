c3ea6fd3971f32c711b5c5c5f7c69a5a700b02a5
sav_slug <yu.donarudo@gmail.com>
2019-10-10 17:23:46 +0900

    RAISE+右上でPRTSCRN

--- keyboards/lily58/keymaps/sslg/keymap.c
+++ keyboards/lily58/keymaps/sslg/keymap.c
@@ -92,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_RAISE] = LAYOUT( \
-  _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, KC_PSCR, \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,  \
   KC_F1,  KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,                       XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, \
   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   _______, _______,  KC_PLUS, KC_MINS, KC_ASTR, KC_LBRC, KC_RBRC, KC_BSLS, \
@@ -243,3 +243,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   }
   return true;
 }
+/*
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
+    // MACRODOWN only works in this function
+    switch (id) {
+        case 0:
+            //SEND_STRING(SS_LCTRL("z") "[");
+            MACRO(D(KC_LALT), T(KC_F4), U(KC_LALT));
+            break;
+    }
+    return MACRO_NONE;
+};
+*/
